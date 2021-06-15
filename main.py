3d99a14d0d61f8d7b54c38bd79421cd2335cd99d
unknown <fesquivel@axedra.com>
2021-06-15 11:24:07 +0100

    cambios

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import socketio
 
-# standard Python
 sio = socketio.Client()
 
+@sio.event
+def my_event(data):
+    print('Received data: ', data)
+    
+# standard Python
+
 sio.connect('http://localhost:8000')
+sio.wait()
