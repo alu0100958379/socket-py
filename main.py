b43496053683c66fd15c0721ec9afd3e6e60956d
Ubuntu <ubuntu@ip-172-31-39-229.eu-west-2.compute.internal>
2021-06-17 17:30:26 +0000

    cambios

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ sio = socketio.Client()
 @sio.event
 def llamadaRecibida(data):
     print('Received data: ', data)
-    
+    if data == "play":
+        print("PULSADA TECLA PLAY")
 # standard Python
 
 sio.connect('http://localhost:8000')
